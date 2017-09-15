cfb208eeb232f80ce0be122ea5bd102edf3954dc
LambdaExpression <13425088053@139.com>
2017-09-15 09:27:05 +0800

    完善了 资源权限类

--- src/main/java/org/tcat/frame/component/ResourcesData.java
+++ src/main/java/org/tcat/frame/component/ResourcesData.java
@@ -13,9 +13,14 @@ import org.tcat.frame.service.gm.GmAdminRepository;
 import org.tcat.frame.service.gm.GmRelAdminRoleRepository;
 import org.tcat.frame.service.gm.GmRelRoleResourceRepository;
 import org.tcat.frame.service.gm.GmRoleRepository;
+import org.tcat.frame.service.gm.dto.AdminDto;
 import org.tcat.frame.service.gm.dto.GmRelAdminRoleDto;
 import org.tcat.frame.service.gm.dto.GmRelRoleResourceDto;
 import org.tcat.frame.service.gm.dto.GmRoleDto;
+import org.tcat.frame.service.gm.enums.AdminDisable;
+import org.tcat.frame.service.gm.enums.AdminGrade;
+import org.tcat.frame.util.PropertiesUtil;
+import org.tcat.frame.util.StringUtils;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
@@ -36,9 +41,38 @@ public final class ResourcesData implements InitializingBean {
     };
     private List<ResourcesDto> resourcesAll = new Vector<>();
     private List<ResourcesDto> resourcesView = new Vector<>();
+    /**
+     * key: resourcesUrl
+     * <br/>value: resourcesId
+     */
+    private Map<String, String> resourcesUrlId = new ConcurrentHashMap<>();
+    /**
+     * key: resourcesId
+     * <br/>value: resourcesUrl
+     */
+    private Map<String, String> resourcesIdUrl = new ConcurrentHashMap<>();
+    /**
+     * key: roleId
+     * <br/>value: resourcesId
+     */
     private Map<Long, Set<String>> roleResources = new ConcurrentHashMap<>();
+    /**
+     * key: resourcesId
+     * <br/>value: roleId
+     */
     private Map<String, Set<Long>> resourcesRole = new ConcurrentHashMap<>();
+    /**
+     * key: adminId
+     * <br/>value: ResourcesDto
+     */
     private Map<Long, List<ResourcesDto>> adminResourcesView = new ConcurrentHashMap<>();
+    /**
+     * key: adminId
+     * <br/>value: roleId
+     */
+    private Map<Long, Set<Long>> adminIdRole = new ConcurrentHashMap<>();
+
+    private static final String ADMINDTO = PropertiesUtil.getValue("config.properties", "resources.adminDto");
 
     @Autowired
     private GmRelRoleResourceRepository gmRelRoleResourceRepository;
@@ -48,6 +82,8 @@ public final class ResourcesData implements InitializingBean {
     private GmAdminRepository gmAdminRepository;
     @Autowired
     private GmRelAdminRoleRepository gmRelAdminRoleRepository;
+    @Autowired
+    private CacheDate cacheDate;
 
     @Override
     public void afterPropertiesSet() throws Exception {
@@ -56,7 +92,6 @@ public final class ResourcesData implements InitializingBean {
     }
 
     public synchronized void reInit() {
-        initResources();
         initRoleResouces();
     }
 
@@ -68,6 +103,8 @@ public final class ResourcesData implements InitializingBean {
 
         resourcesAll.clear();
         resourcesView.clear();
+        resourcesUrlId.clear();
+        resourcesIdUrl.clear();
 
         for (String resource : resources) {
             InputStream inputStream = ResourcesData.class.getClassLoader().getResourceAsStream("resources" + File.separator + resource);
@@ -76,8 +113,9 @@ public final class ResourcesData implements InitializingBean {
                     , new TypeToken<ResourcesDto>() {
                     }.getType());
             resourcesAll.add(resourcesDto);
+            initResourcesUrl(resourcesDto);
         }
-        resourcesView = this.getViewResources(resourcesAll, null);
+        resourcesView = this.getViewResources(resourcesAll, ResourcesType.menu, null);
     }
 
 
@@ -112,21 +150,49 @@ public final class ResourcesData implements InitializingBean {
     }
 
     /**
+     * 初始化 资源数据
+     *
+     * @param resourcesDto 资源数据
+     */
+    private synchronized void initResourcesUrl(ResourcesDto resourcesDto) {
+        if (resourcesDto == null) {
+            return;
+        }
+        if (StringUtils.isEmptyByTrim(resourcesDto.getId()) || StringUtils.isEmptyByTrim(resourcesDto.getUrl())) {
+            throw new RuntimeException("Error: 资源数据不完整，ID 或 Url 为空\n\n" + new Gson().toJson(resourcesDto) + "\n\n");
+        }
+        if (resourcesUrlId.get(resourcesDto.getUrl()) != null) {
+            throw new RuntimeException("Error: 资源url：" + resourcesDto.getUrl() + " 重复\n\n" + new Gson().toJson(resourcesDto) + "\n\n");
+        }
+        if (resourcesIdUrl.get(resourcesDto.getId()) != null) {
+            throw new RuntimeException("Error: 资源Id：" + resourcesDto.getId() + " 重复\n\n" + new Gson().toJson(resourcesDto) + "\n\n");
+        }
+        resourcesUrlId.put(resourcesDto.getUrl(), resourcesDto.getId());
+        resourcesIdUrl.put(resourcesDto.getId(), resourcesDto.getUrl());
+        if (resourcesDto.getChild() != null) {
+            for (ResourcesDto dto : resourcesDto.getChild()) {
+                initResourcesUrl(dto);
+            }
+        }
+    }
+
+    /**
      * 筛选 菜单资源
      *
      * @param resourcesList 资源
+     * @param type          类型(可为空，为空时，不做类型筛选)
      * @param purview       可查看的权限（可为空）
      * @return 菜单资源集合
      */
-    private synchronized List<ResourcesDto> getViewResources(List<ResourcesDto> resourcesList, Set<String> purview) {
+    private synchronized List<ResourcesDto> getViewResources(List<ResourcesDto> resourcesList, ResourcesType type, Set<String> purview) {
         if (resourcesList == null) {
             return null;
         }
         List<ResourcesDto> resourcesViewList = new Vector<>();
         for (ResourcesDto resourcesDto : resourcesList) {
-            if (Integer.valueOf(ResourcesType.menu.value()).equals(resourcesDto.getType())
+            if ((type == null || Integer.valueOf(type.value()).equals(resourcesDto.getType()))
                     && (purview == null || purview.contains(resourcesDto.getId()))) {
-                resourcesDto.setChild(getViewResources(resourcesDto.getChild(), purview));
+                resourcesDto.setChild(getViewResources(resourcesDto.getChild(), type, purview));
                 resourcesViewList.add(resourcesDto);
             }
         }
@@ -135,28 +201,56 @@ public final class ResourcesData implements InitializingBean {
     }
 
     /**
-     * 获取用户的菜单列表
+     * 获取 全部资源列表
+     *
+     * @return 全部资源列表
+     */
+    public synchronized List<ResourcesDto> getResourcesAll() {
+        return resourcesAll;
+    }
+
+    /**
+     * 获取 用户的菜单列表
      *
      * @param adminId 用户id
      * @return 菜单列表
      */
     public synchronized List<ResourcesDto> getViewResourcesForAdminId(Long adminId) {
+        return getResourcesForAdminIdAndType(adminId, ResourcesType.menu);
+    }
+
+    /**
+     * 获取 用户的资源列表
+     *
+     * @param adminId 用户id
+     * @return 资源列表
+     */
+    public synchronized List<ResourcesDto> getResourcesForAdminId(Long adminId) {
+        return getResourcesForAdminIdAndType(adminId, null);
+    }
+
+    /**
+     * 获取 用户的资源列表
+     *
+     * @param adminId 用户id
+     * @param type    资源类型（为空时不做筛选）
+     * @return 资源列表
+     */
+    private synchronized List<ResourcesDto> getResourcesForAdminIdAndType(Long adminId, ResourcesType type) {
         List<ResourcesDto> rList = adminResourcesView.get(adminId);
         if (rList == null || rList.size() == 0) {
-            List<GmRelAdminRoleDto> rarList = gmRelAdminRoleRepository.findByAdminId(adminId);
-            if (rarList != null && rarList.size() > 0) {
-                Set<String> purview = new HashSet<>();
-                for (GmRelAdminRoleDto gmRelAdminRoleDto : rarList) {
-                    Set<String> pu = roleResources.get(gmRelAdminRoleDto.getRoleId());
-                    if (pu != null && pu.size() > 0) {
-                        purview.addAll(pu);
-                    }
+            Set<Long> roleIdSet = getRoleIdForAdminId(adminId);
+            Set<String> purview = new HashSet<>();
+            for (Long roleId : roleIdSet) {
+                Set<String> pu = roleResources.get(roleId);
+                if (pu != null && pu.size() > 0) {
+                    purview.addAll(pu);
                 }
-                if (purview.size() > 0) {
-                    List<ResourcesDto> resList = this.getViewResources(resourcesView, purview);
-                    if (resList != null && resList.size() > 0) {
-                        rList = resList;
-                    }
+            }
+            if (purview.size() > 0) {
+                List<ResourcesDto> resList = this.getViewResources(resourcesView, type, purview);
+                if (resList != null && resList.size() > 0) {
+                    rList = resList;
                 }
             }
             if (rList == null) {
@@ -168,6 +262,79 @@ public final class ResourcesData implements InitializingBean {
         return rList;
     }
 
-//    public synchronized boolean hasPermissions(Long adminId,String url)
+    /**
+     * 根据用户id 获取 角色id
+     *
+     * @param adminId 用户id
+     * @return 角色id集合
+     */
+    private synchronized Set<Long> getRoleIdForAdminId(Long adminId) {
+        Set<Long> roleIdSet = adminIdRole.get(adminId);
+        if (roleIdSet == null) {
+            roleIdSet = new HashSet<>();
+            List<GmRelAdminRoleDto> rarList = gmRelAdminRoleRepository.findByAdminId(adminId);
+            if (rarList != null && rarList.size() > 0) {
+                for (GmRelAdminRoleDto gmRelAdminRoleDto : rarList) {
+                    roleIdSet.add(gmRelAdminRoleDto.getRoleId());
+                }
+            }
+            if (roleIdSet.size() > 0) {
+                adminIdRole.put(adminId, roleIdSet);
+            }
+        }
+        return roleIdSet;
+    }
+
+    /**
+     * 判断用户是否有访问该url的权限
+     *
+     * @param adminId 用户id
+     * @param url     url资源
+     * @return turn 有权限 / false 无权限
+     */
+    public synchronized boolean hasPermissions(Long adminId, String url) {
+        if (adminId == null || StringUtils.isEmptyByTrim(url)) {
+            return false;
+        }
+        AdminDto adminDto = this.getAdminDto(adminId);
+        if (adminDto == null || AdminDisable.YES.equals(adminDto.getDisable())) {
+            return false;
+        }
+        if (AdminGrade.SUPER.equals(adminDto.getGrade())) {
+            return true;
+        }
+        String resourcesId = resourcesUrlId.get(url);
+        if (StringUtils.isEmptyByTrim(resourcesId)) {
+            return false;
+        }
+        Set<Long> roleIdSet = getRoleIdForAdminId(adminId);
+        for (Long roleId : roleIdSet) {
+            Set<String> roleSet = roleResources.get(roleId);
+            if (roleSet != null && roleIdSet.contains(resourcesId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 获取 用户Dto
+     *
+     * @param adminId 用户Id
+     * @return 用户Dto
+     */
+    private synchronized AdminDto getAdminDto(Long adminId) {
+        if (adminId == null) {
+            return null;
+        }
+        AdminDto adminDto = cacheDate.get(ADMINDTO + adminId, AdminDto.class);
+        if (adminDto == null) {
+            adminDto = gmAdminRepository.getOne(adminId);
+            if (adminDto != null) {
+                cacheDate.put(ADMINDTO + adminId, adminDto);
+            }
+        }
+        return adminDto;
+    }
 
 }
